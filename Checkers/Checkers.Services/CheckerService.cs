737530de6eabd5064b7045083cc7bdfa031f26f8
Cameron Scott Graybill <camerongraybill@gmail.com>
2018-06-28 19:21:47 -0400

    Got code coverage working! (#4)
    * Messing with builds to see what I can make happen
    
    * work?
    
    * Maybe builds with core now?
    
    * Update .travis.yml
    
    * Adding First Unit Test
    
    * Build both mono and core?
    
    * Add tests with coverage and base projects
    
    * code coverage?
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Merge branches 'master' and 'testing-rider-solution' of github.com:cs451-ducks/checkers into testing-rider-solution
    
    # Conflicts:
    #	.travis.yml
    #	Checkers/Checkers.sln
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update README.md

---
+++ Checkers/Checkers.Services/CheckerService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Checkers.Models;
+
+namespace Checkers.Services
+{
+    public class CheckerService
+    {
+        public Checker MakeChecker()
+        {
+            Checker c = new Checker();
+
+            c.X = 5;
+            c.Y = 0;
+
+            return c;
+        }
+    }
+}
---
+++ Checkers/Checkers.Test/DemoTest.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+//Adam Feldscher
+namespace CheckersTest
+{
+    public class DemoTest
+    {
+        [Fact]
+        public void CamTest()
+        {
+            bool isCamDumb = true;
+            Assert.True(isCamDumb);
+        }
+    }
+}
