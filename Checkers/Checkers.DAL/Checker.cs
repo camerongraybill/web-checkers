418e590e6a60a63e5a908ae13586d6a6094d94a3
Adam Feldscher <aff39@drexel.edu>
2018-06-28 17:01:06 -0400

    Add tests with coverage and base projects

---
+++ Checkers/Checkers.DAL/Checker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Checkers.Models
+{
+    public class Checker
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
