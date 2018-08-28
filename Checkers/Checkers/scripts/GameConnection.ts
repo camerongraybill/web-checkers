2badc373f188f4281422ae1982b273df91137e20
Cameron Graybill <csg58@drexel.edu>
2018-08-28 13:32:06 -0400

    fixes #12

--- Checkers/Checkers/scripts/GameConnection.ts
+++ Checkers/Checkers/scripts/GameConnection.ts
@@ -1,4 +1,4 @@
-import *  as signalR from "@aspnet/signalr";
+import * as signalR from "@aspnet/signalr";
 import $ = require("jquery");
 import {Action} from "./DataTransferObjects/Action";
 import {EndGame} from "./DataTransferObjects/EndGame";
@@ -14,7 +14,8 @@ const GAME_CONNECTION_URL = "/game";
 export class GameConnection {
     private readonly board: Board;
     private readonly connection: signalR.HubConnection = null;
-    private _my_color: Color = null;
+    private innerMyColor: Color = null;
+    private gameEnded: boolean = false;
 
     constructor(board: Board) {
         this.board = board;
@@ -22,32 +23,28 @@ export class GameConnection {
         this.initializeCallbacks();
     }
 
-    private get my_color(): Color {
-        return this._my_color;
-    }
-
     private set my_color(value: Color) {
-        const team_screen = $("#oTeam");
         switch (value) {
             case Color.BLACK:
-                team_screen.text("BLACK TEAM");
+                $("#oTeam").text("BLACK TEAM");
                 break;
             case Color.RED:
-                team_screen.text("RED TEAM");
+                $("#oTeam").text("RED TEAM");
                 break;
         }
-        this._my_color = value;
+        this.innerMyColor = value;
     }
 
     public start(): void {
-        this.connection.start().catch((err) => console.error(err)).then(() => {
+        this.connection.start().then(() => {
             $("#landingPage").addClass("hiddenPage closedPage");
             $("#publicMatchPage").removeClass("hiddenPage closedPage");
         });
     }
 
     private initializeCallbacks(): void {
-        this.connection.on("gameStart", (data: string) => this.on_game_start(StartGame.decode(data)));
+        this.connection
+            .on("gameStart", (data: string) => this.on_game_start(StartGame.decode(data)));
         this.connection.on("yourMove", (data: string) => this.on_your_turn(Turn.decode(data)));
         this.connection.on("gameEnd", (data: string) => this.on_game_end(EndGame.decode(data)));
         this.board.on_move_callback = (from: BoardLocation, to: BoardLocation) => this.sendMove(from, to);
@@ -55,39 +52,36 @@ export class GameConnection {
 
     private on_game_end(data: EndGame): void {
         // Do something with the winner and reason
-        const end_reason: GameEndReason = data.reason;
-        const winner: Color = data.winner;
-        const winner_color = (() => {
-            switch (winner) {
-                case Color.RED: return "red";
-                case Color.BLACK: return "black";
+        if (!this.gameEnded) {
+            const endGameReason: GameEndReason = data.reason;
+            const winner: Color = data.winner;
+            if (endGameReason === GameEndReason.OPPONENT_DISCONNECT) {
+                $("#oEnd").text("Your opponent disconnected");
+            } else if (winner === this.innerMyColor) {
+                $("#oEnd").text("You won!");
+            } else {
+                $("#oEnd").text("You lost");
             }
-        })();
-        if (end_reason === GameEndReason.OPPONENT_DISCONNECT) {
-            console.log("Your opponent Disconnected");
+
+            $("#gamePage").addClass("hiddenPage closedPage");
+            $("#endPage").removeClass("hiddenPage closedPage");
+            this.gameEnded = true;
         }
-        $("#oEnd").text(winner_color + " won!");
-        $("#gamePage").addClass("hiddenPage closedPage");
-        $("#endPage").removeClass("hiddenPage closedPage");
     }
 
     private on_game_start(data: StartGame): void {
-        if (this.my_color !== null) {
-            console.error("Got game start message when game was already started");
-        } else {
+        if (this.innerMyColor === null) {
             $("#totalContainer").addClass("hiddenPage closedPage");
             $("#gamePage").removeClass("hiddenPage closedPage");
-            const new_board: Board = Board.fromJSON(data.raw_board);
-            this.board.updateFromOtherBoard(new_board);
-            this.my_color = data.color;
+            this.board.updateFromOtherBoard(Board.fromJSON(data.rawBoard));
+            this.innerMyColor = data.color;
         }
     }
 
     private on_your_turn(data: Turn): void {
-        this.board.updateFromOtherBoard(Board.fromJSON(data.raw_board));
-        const possible_moves = data.raw_moves
-            .map((raw_move_object: any) => Move.fromJSON(raw_move_object, this.board));
-        this.board.legal_moves = possible_moves;
+        this.board.updateFromOtherBoard(Board.fromJSON(data.rawBoard));
+        this.board.legal_moves = data.rawMoves
+            .map((rawMoveObject: any) => Move.fromJSON(rawMoveObject, this.board));
         $("#oTeam").addClass("activePlayer");
     }
 
